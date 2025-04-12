a3555b82dcbcb06fe4ea3f0b5b9ddb10f53fd498
CarlosVillaalba <104330333+CarlosVillaalba@users.noreply.github.com>
2025-04-12 15:02:02 -0400

    Add files via upload

---
+++ arrayExercise.cs
@@ -0,0 +1,16 @@
+using System;
+namespace EjercicioDeArrays
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int [] array ={1,2,3,4,5,6,7,8,9,10};
+            for (int i=0;i<array.leght;i++);
+            {
+                Console.WriteLine(array[i])
+            }
+            Console.ReadLine();
+        }
+    }
+}
