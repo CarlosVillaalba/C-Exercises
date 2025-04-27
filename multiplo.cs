d7ec1dcfec63f648b82ec830e24f6d78784f7215
CarlosVillaalba <104330333+CarlosVillaalba@users.noreply.github.com>
2025-04-27 11:29:23 -0400

    Add files via upload

---
+++ multiplo.cs
@@ -0,0 +1,9 @@
+using System;
+class Program{
+    void static Main(string[]args){
+        for (i=0;i<500;i++){
+            Console.WriteLine("multiplos de 5 y3"+i*15);
+        }
+        Console.ReadKey();
+    }
+}
